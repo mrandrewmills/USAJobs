4cc4c159a02903888d8206cea328a6232de535ec
Andrew Mills <AndrewMills@knowwwhow.com>
2015-10-03 08:47:42 -0400

    fix typos and errors
    replaced missing $ on variables, added missing curly bracket.

--- USAJobs.class.php
+++ USAJobs.class.php
@@ -10,45 +10,45 @@
      private $baseURL = "https://data.usajobs.gov/api/search";
 
      // required request headers
-     private host = "data.usajobs.gov";
-     private user_agent;
-     private authorization_key;
+     private $host = "data.usajobs.gov";
+     private $user_agent;
+     private $authorization_key;
 
      // optional query parameters
-     private keyword;
-     private keywordExclusion;
-     private keywordFilter;
-     private PositionTitle;
-     private remunerationMinimumAmount;
-     private remunerationMaximumAmount;
-     private payGradeHigh;
-     private payGradeLow;
-     private jobCategoryCode;
-     private locationName;
-     private postingChannel;
-     private organization;
-     private positionOfferingTypeCode;
-     private travelPercentage;
-     private positionScheduleTypeCode;
-     private relocationIndicator;
-     private securityClearanceRequired;
-     private supervisoryStatus;
-     private excludeJOAOpenFor30Days;
-     private datePosted;
-     private jobGradeCode;
-     private sortField;
-     private sortDirection;
-     private page;
-     private resultsPerPage;
-     private whoMayApply;
-     private radius;
-     private fields;
-     private salaryBucket;
-     private gradeBucket;
-     private SES;
-     private Student;
-     private Internship;
-     private recentGrad;
+     private $keyword;
+     private $keywordExclusion;
+     private $keywordFilter;
+     private $PositionTitle;
+     private $remunerationMinimumAmount;
+     private $remunerationMaximumAmount;
+     private $payGradeHigh;
+     private $payGradeLow;
+     private $jobCategoryCode;
+     private $locationName;
+     private $postingChannel;
+     private $organization;
+     private $positionOfferingTypeCode;
+     private $travelPercentage;
+     private $positionScheduleTypeCode;
+     private $relocationIndicator;
+     private $securityClearanceRequired;
+     private $supervisoryStatus;
+     private $excludeJOAOpenFor30Days;
+     private $datePosted;
+     private $jobGradeCode;
+     private $sortField;
+     private $sortDirection;
+     private $page;
+     private $resultsPerPage;
+     private $whoMayApply;
+     private $radius;
+     private $fields;
+     private $salaryBucket;
+     private $gradeBucket;
+     private $SES;
+     private $Student;
+     private $Internship;
+     private $recentGrad;
 
      function __constructor($ua, $apikey){
 
@@ -70,5 +70,33 @@
        $this->authorization_key = $apikey;
      }
 
+     function getJobListing() {
+       if (($this->user_agent != "") && ($this->authorization_key != "")) {
+
+          // establish our options (headers, etc.)
+          $opts = array(
+            'http'=>array(
+              'method'=>"GET",
+              'header'=>"Host: $this->host\r\n" .
+                        "User-Agent: $this->user_agent\r\n" .
+                        "Authorization-Key: $this->authorization_key"
+            )
+          );
+
+          // convert our options into a context
+          $context = stream_context_create($opts);
+
+          // retrieve JSON of jobs listing with our options/context
+          $file = file_get_contents($this->baseURL, false, $context);
+
+       } else {
+
+         // TODO: throw an error about missing required item.
+       }
+     }
+
+     function parseJobListing() {
+       //TODO: parse the JSON response into an object
+     }
    }
 ?>
